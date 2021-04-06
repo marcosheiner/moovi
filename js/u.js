9ae0ad0e8ccf7feec4b0de185e6b46815a1b4196
marcosheiner <marcosheiner2000@gmail.com>
2021-04-06 17:20:09 -0300

    js protect

---
+++ js/u.js
@@ -0,0 +1,25 @@
+
+if (document.addEventListener) {
+    document.addEventListener("keydown", bloquearSource);
+} else { //Versões antigas do IE
+    document.attachEvent("onkeydown", bloquearSource);
+}
+
+function bloquearSource(e) {
+    e = e || window.event;
+
+    var code = e.which || e.keyCode;
+
+    if (
+        e.ctrlKey &&
+        (code == 83 || code == 85) //83 = S, 85 = U
+    ) {
+        if (e.preventDefault) {
+            e.preventDefault();
+        } else {
+            e.returnValue = false;
+        }
+
+        return false;
+    }
+}
